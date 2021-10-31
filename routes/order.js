aa1d7db6dfc861ae7dd910989dc16bd2b39126d6
sparks-11 <84897249+sparks-11@users.noreply.github.com>
2021-10-31 13:59:36 +0530

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -39,7 +39,7 @@ router.delete("/delete/:id", verfiyTokenAndAdmin, async (req, res) => {
 })
 
 // GET USER ORDER
-router.get("/find/:userId", verfiyTokenAndAuthorization, async (req, res) => {
+router.get("/find/:userId", verfiyToken, async (req, res) => {
   try {
   const orders = await Order.find({userId:req.params.userId})
       res.status(200).json(orders)
